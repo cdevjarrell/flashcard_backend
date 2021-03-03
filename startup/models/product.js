f995215aa22394e13064ba557613f5fa8a44bf9f
cdevjarrell <christian.j.jarrell@gmail.com>
2021-03-03 18:56:26 -0500

    Create models folder and product.js file for Schema.

---
+++ startup/models/product.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 255 },
+  description: { type: String, required: true },
+  category: { type: String, required: true, minlegnth: 5, maxlength: 50 },
+  price: { type: Number, required: true },
+  dateModified: { type: Date, default: Date.now },
+});
